19e0480c802fd0b6d98e8623d14d79bff2b42bb8
Olya Y <f_akora@mail.ru>
2023-07-22 20:41:03 +0300

    feat: expand user type

--- src/routes/graphql/types/user.ts
+++ src/routes/graphql/types/user.ts
@@ -2,4 +2,12 @@ export type IUser = {
   id: string;
   name: string;
   balance: number;
+  userSubscribedTo?: {
+    subscriberId: string;
+    authorId: string;
+  }[];
+  subscribedToUser?: {
+    subscriberId: string;
+    authorId: string;
+  }[];
 };
